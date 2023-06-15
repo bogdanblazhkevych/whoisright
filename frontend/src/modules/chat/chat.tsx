4e813b4d6b604269429d12005a941a0be53dd409
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-06-15 18:19:25 -0400

    messages from convo stored in state as object[]

--- frontend/src/modules/chat/chat.tsx
+++ frontend/src/modules/chat/chat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Textinput from "../textinput/textinput";
 import Textoutput from "../textoutput/textoutput";
 import chatcss from "./chat.module.css"
@@ -8,6 +8,7 @@ interface ServerToClientEvents {
   noArg: () => void;
   basicEmit: (a: number, b: string, c: Buffer) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
+  receive_message: (a: ChatMessageInterface) => void;
 }
 
 interface ClientToServerEvents {
@@ -15,9 +16,24 @@ interface ClientToServerEvents {
   send_message: (message: string) => void;
 }
 
+interface ChatMessageInterface {
+    // sender: string;
+    message: string
+}
+
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:8000');
 
 export default function Chat(){
+
+    const [messageLog, setMessageLog] = useState<ChatMessageInterface[]>([]);
+
+    useEffect(() => {
+        socket.on('receive_message', (message) => {
+            setMessageLog((previous) => [...previous, message])
+            console.log(messageLog)
+            console.log(message)
+        })
+    }, [socket])
 
     function sendMessage(message: string) {
         socket.emit("send_message", message)
