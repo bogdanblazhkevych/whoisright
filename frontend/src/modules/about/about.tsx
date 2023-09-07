229aa567447f550b23ffd3f315989e3f235772df
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-09-07 12:05:46 -0400

    about page now can be toggled on mobile devices

--- frontend/src/modules/about/about.tsx
+++ frontend/src/modules/about/about.tsx
@@ -4,75 +4,57 @@ import aboutcss from './aboutcss.module.css'
 export default function About() {
     return (
         <div className={aboutcss.aboutwrapper}>
-            <div className={aboutcss.heading}>
-                WHOISRIGHT.ME
-            </div>
+            {/* <div className={aboutcss.aboutcontainer}> */}
+                <div className={aboutcss.heading}>
+                    WHOISRIGHT.ME
+                </div>
 
-            {/* <div className={aboutcss.body} style={{textAlign: 'center'}}>
-                Welcome to WhoIsRight.me, your go-to platform for resolving disputes and conflicts in a fair and unbiased manner. We believe in fostering constructive conversations and finding common ground, all while harnessing the power of cutting-edge technology.
-            </div> */}
+                <div className={aboutcss.subheading}>
+                    How It Works
+                </div>
 
-            {/* <div className={aboutcss.subheading}>
-                Our Mission
-            </div>
+                <div className={aboutcss.body}>
+                    WhoIsRight.me connects users through websockets, allowing them to engage in text-based conversations. But what sets us apart is our AI Conflict Mediator. This innovative feature steps in when conversations get heated or conflicts arise, with the goal of finding a resolution that both parties can agree upon.
+                </div>
 
-            <div className={aboutcss.body}>
-                Our mission is simple: to facilitate open dialogue and promote understanding between individuals with differing opinions. In a world full of discord and division, WhoIsRight.me strives to bridge the gaps and bring people together.
-            </div> */}
+                <div className={aboutcss.subheading}>
+                    Meet Our AI Conflict Mediator
+                </div>
 
-            <div className={aboutcss.subheading}>
-                How It Works
-            </div>
+                <div className={aboutcss.body}>
+                    Our AI Conflict Mediator is powered by GPT-3.5 Turbo, the latest advancement in AI language models. It's designed to remain impartial, using data-driven insights to mediate discussions and provide well-reasoned suggestions. Think of it as a digital peacemaker, here to ensure a balanced and productive exchange of ideas.
+                </div>
 
-            <div className={aboutcss.body}>
-                {/* Select <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Create Session</span> to create a chat room. Relay the generated session code to the opposing party. */}
-                WhoIsRight.me connects users through websockets, allowing them to engage in text-based conversations. But what sets us apart is our AI Conflict Mediator. This innovative feature steps in when conversations get heated or conflicts arise, with the goal of finding a resolution that both parties can agree upon.
-            </div>
+                <div className={aboutcss.subheading}>
+                    Why Chose WHOISRIGHT.ME?
+                </div>
 
-            <div className={aboutcss.subheading}>
-                Meet Our AI Conflict Mediator
-            </div>
+                <div className={aboutcss.body} style={{color: "#E1E1E3"}}>
+                    <ul>
+                        <li>
+                            <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Fairness:</span> <span style={{color:'#9f9fa0'}}>We're committed to fairness and impartiality. Our AI Conflict Mediator ensures that all parties get a fair chance to express their views.</span>
+                        </li>
+                        <li>
+                        <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Privacy:</span> <span style={{color:'#9f9fa0'}}>We prioritize your privacy. We do not collect any personal data and and once your discussions conclude, they are promptly and permanently deleted from our platform. </span>
+                        </li>
+                        <li>
+                        <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Empowerment:</span> <span style={{color:'#9f9fa0'}}>WhoIsRight.me empowers you to engage in meaningful conversations, explore different perspectives, and ultimately, find common ground.</span>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div className={aboutcss.subheading}>
+                    Commitment to Open Source
+                </div>
+                
+                <div className={aboutcss.body}>
+                    We believe that transparency, collaboration, and community involvement are essential for the growth and improvement of our platform. That's why we are committed to open sourcing as much of our codebase as possible. 
+                </div>
 
-            <div className={aboutcss.body}>
-                Our AI Conflict Mediator is powered by GPT-3.5 Turbo, the latest advancement in AI language models. It's designed to remain impartial, using data-driven insights to mediate discussions and provide well-reasoned suggestions. Think of it as a digital peacemaker, here to ensure a balanced and productive exchange of ideas.
-            </div>
-
-            <div className={aboutcss.subheading}>
-                Why Chose WHOISRIGHT.ME?
-            </div>
-
-            <div className={aboutcss.body} style={{color: "#E1E1E3"}}>
-                <ul>
-                    <li>
-                        <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Fairness:</span> <span style={{color:'#9f9fa0'}}>We're committed to fairness and impartiality. Our AI Conflict Mediator ensures that all parties get a fair chance to express their views.</span>
-                    </li>
-                    <li>
-                    <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Privacy:</span> <span style={{color:'#9f9fa0'}}>We prioritize your privacy. We do not collect any personal data and and once your discussions conclude, they are promptly and permanently deleted from our platform. </span>
-                    </li>
-                    <li>
-                    <span style={{ color: "#E1E1E3", fontWeight: 800 }}>Empowerment:</span> <span style={{color:'#9f9fa0'}}>WhoIsRight.me empowers you to engage in meaningful conversations, explore different perspectives, and ultimately, find common ground.</span>
-                    </li>
-                </ul>
-            </div>
-            
-            <div className={aboutcss.subheading}>
-                Commitment to Open Source
-            </div>
-            
-            <div className={aboutcss.body}>
-                We believe that transparency, collaboration, and community involvement are essential for the growth and improvement of our platform. That's why we are committed to open sourcing as much of our codebase as possible. 
-            </div>
-
-            {/* <div className={aboutcss.subheading}>
-                GitHub Repo: https://github.com/bogdanblazhkevych/whoisright
-            </div> */}
-
-            <a className={`${aboutcss.subheading} ${aboutcss.link}`} href='https://github.com/bogdanblazhkevych/whoisright' about="_blank">
-                GitHub
-            </a>
-            {/* <div className={aboutcss.body}>
-            At WhoIsRight.me, we believe that dialogue is the first step towards understanding, and understanding is the path to resolution. Join our growing community of users who are committed to finding common ground and working through their differences constructively.
-            </div> */}
+                <a className={`${aboutcss.subheading} ${aboutcss.link}`} href='https://github.com/bogdanblazhkevych/whoisright' about="_blank">
+                    GitHub
+                </a>
+            {/* </div> */}
         </div>
     )
 }
